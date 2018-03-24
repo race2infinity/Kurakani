47195d41537d13cd098dce05eed4a44056289440
shadrak98 <30501401+shadrak98@users.noreply.github.com>
2018-03-24 23:17:34 +0530

    Just a demo of website

---
+++ routes/users.js
@@ -0,0 +1,125 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('passportapp', ['users']);
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+//Login Page - Get
+router.get('/login', function(req, res){
+    res.render('login');
+});
+
+//Register Page -Get
+router.get('/register', function(req, res){
+    res.render('register');
+});
+
+//Register - POST
+router.post('/register', function(req, res){
+    // Get form values
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    // validation
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('email', 'Email field is required').notEmpty();    
+    req.checkBody('email', 'please use a valid email address').isEmail();
+    req.checkBody('username', 'Username field is required').notEmpty();    
+    req.checkBody('password', 'Password field is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    
+    var errors = req.validationErrors();
+
+    if(errors){
+        console.log('Form has erors....');
+        res.render('register',{
+            errors: errors,
+            name: name,
+            email: email,
+            username: username,
+            password: password,
+            password2: password2
+        });
+    } else {
+        var newUser = {
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        }
+        
+        bcrypt.genSalt(10, function(err, salt){
+            bcrypt.hash(newUser.password, salt, function(err, hash){
+                newUser.password = hash;
+                db.users.insert(newUser, function(err, doc){
+                    if(err){
+                        res.send(err);
+                    } else {
+                        console.log('User Added...');
+        
+                        //Success Message
+                        req.flash('success', 'You are registered and can now log in');
+        
+                        //Redirect after register
+                        res.location('/');
+                        res.redirect('/');
+                    }
+                });
+            });
+        });
+
+        
+    }
+});
+
+passport.serializeUser(function(user, done){
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    db.users.findOne({_id: mongojs.ObjectId(id)}, function(err,user) {
+        done(err, user); 
+    });
+});
+
+passport.use(new LocalStrategy(function(username, password, done){
+    db.users.findOne({username: username}, function(err, user) {
+            if(err) {
+                return done(err);
+            }
+            if(!user){
+                return done(null, false, {message: 'Incorrect Username'});
+            }
+
+            bcrypt.compare(password, user.password, function(err, isMatch){
+                if(err) {
+                    return done(err);
+                }
+                if(isMatch){
+                    return done(null, user);
+                }else {
+                    return done(null, false,{message: 'Incorrect Password'})
+                }
+            });
+    });
+}));
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/', failureRediect: '/users/login', failureFlash: 'Invalid Username or Password' }), 
+    function(req, res){
+        console.log('Authentication successful');
+        res.redirect('/');  
+});
+
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success', 'You have logged out');
+    req.redirect('/users/login');
+})
+
+module.exports = router;
