1a525e12cd46112185c29d15f90105b4bbd076b1
shadrak98 <30501401+shadrak98@users.noreply.github.com>
2018-03-25 22:23:14 +0530

    web part is merged

---
+++ Chatapp_web/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', ensureAuthenticated, function(req, res){
+    res.render('index');
+    console.log('this is index page')
+});
+
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/users/login');
+}
+
+module.exports = router;
